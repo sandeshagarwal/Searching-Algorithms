768be06a3c19aad5e07973bf72749e9a9d2cfbd4
sandeshagarwal <sandeshagarwal10@gmail.com>
2021-01-11 16:36:59 +0530

    P5 - Index of last occurence in sorted array(Iter)
    Index of first occurrence of an element in a sorted array(Iterative)

---
+++ Program5.java
@@ -0,0 +1,43 @@
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		int arr[] = {1,10,10,10,20,20,40};
+		int x = 20; //number to be find
+		int length = arr.length;
+		int low = 0;
+		int high = length-1;
+		int result = BinarySearch(arr,x,low,high);
+		System.out.println("the position of "+x+" is "+result);
+		
+	}
+	static int BinarySearch(int arr[],int x,int low,int high)
+	{
+	    
+	    while (low <= high)
+	    {
+	        int mid =(low+(high))/2;
+	       if(arr[mid] == x)
+	        {
+	            if(mid == 0 || arr[mid] != arr[mid+1])
+	            {
+	                return mid;
+	            }
+	            else
+	                low = mid + 1;
+	        }
+	        if(arr[mid] < x)
+	        {
+	            low= mid+1;
+	        }
+	        else if(arr[mid] > x)
+	        {
+	            high = mid - 1;
+	        }
+	       // else{ // arr[mid] == x
+	            
+	       // }
+	    }
+	    return -1;
+	}
+}
